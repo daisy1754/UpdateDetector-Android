02d77be695dac4c1552f64402b559ae4791d153e
daisy1754 <d-part@hotmail.co.jp>
2016-02-15 10:02:33 -0800

    Initial commit with empty library project and skelton of sample project

---
+++ sample/src/main/java/jp/gr/java_conf/daisy/update_detector/updatedetector/sample/CheckUpdateActivity.java
@@ -0,0 +1,14 @@
+package jp.gr.java_conf.daisy.update_detector.updatedetector.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CheckUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.check_update_activity);
+        // TODO
+    }
+}
